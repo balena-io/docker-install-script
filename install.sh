0f58dab6ce5b703f77d0fac49924a91a7203482f
bullshit <me@oskarholowaty.com>
2014-04-04 23:30:45 +0200

    Update install.sh
    Remove PKGBUILD download and install it thru pacman, because the docker PKGBUILD is obsolete

--- install.sh
+++ install.sh
@@ -19,15 +19,13 @@ PACKAGE=aufs3-util-20130907-1-armv6h.pkg.tar.xz
 echo "Downloading $PACKAGE"
 curl --progress-bar -L https://github.com/resin-io/aufs3-util-PKGBUILD/releases/download/v20130907-1/$PACKAGE > $TMP_DIR/$PACKAGE
 
-PACKAGE=docker-0.8.0-1-armv6h.pkg.tar.xz
-echo "Downloading $PACKAGE"
-curl --progress-bar -L https://github.com/resin-io/lxc-docker-PKGBUILD/releases/download/v0.8.0-1/$PACKAGE > $TMP_DIR/$PACKAGE
-
 # Install packages
 yes | pacman -U $TMP_DIR/linux-headers-raspberrypi-aufs_friendly-3.10.19-1-armv6h.pkg.tar.xz \
     $TMP_DIR/linux-raspberrypi-aufs_friendly-3.10.19-1-armv6h.pkg.tar.xz \
-    $TMP_DIR/aufs3-util-20130907-1-armv6h.pkg.tar.xz \
-    $TMP_DIR/docker-0.8.0-1-armv6h.pkg.tar.xz
+    $TMP_DIR/aufs3-util-20130907-1-armv6h.pkg.tar.xz
+    
+# intall docker
+yes | pacman -Sy docker
 
 # Cleanup
 rm -rf $TMP_DIR
